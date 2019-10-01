b1f727a98774a4e1c27bd7eaf479d1f6feb5686f
Ruby1707 <53220616+Ruby1707@users.noreply.github.com>
2019-10-02 01:28:52 +0300

    Result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let NewMass=[];
+var a="";
+var b="";
+Index1=0;
+Index2=0;
+var Result=true;
+for(var i=0;i<=str.length-1;i++){
+  NewMass[i]=str.substr(i,1);
+}
+for(var j=0;j<=bracketsConfig.length-1;j++){
+    a=bracketsConfig[j][0];
+    b=bracketsConfig[j][1];
+    for(var i=0;i<=NewMass.length-1;i++){
+      if(a==b){
+      Index1=NewMass.indexOf(a,0);
+      Index2=NewMass.lastIndexOf(a,Index1+1);
+      }
+      else{
+      Index1=NewMass.indexOf(b,0);
+      Index2=NewMass.lastIndexOf(a,Index1);
+      }
+      if(Index1>=0 &&Index2>=0){
+        if((Index1-Index2-1)%2==0){
+          NewMass.splice(Index2,1);
+          NewMass.splice(Index1,1);
+          }
+        else{
+          Result=false;
+          i=NewMass.length+1;
+          }
+        i--;
+        }
+    }
+  }
+  if(NewMass.length>0){
+    Result=false;
+  }
+  return Result;
 }
